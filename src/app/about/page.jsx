48c95f40eebf40cf6fc122fca8e2516bf5fc805d
Angelina Marchuk <156038035+Angeli13101@users.noreply.github.com>
2024-04-16 17:12:00 -0600

    Update page.jsx
    ghj

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -49,13 +49,13 @@ export default function About() {
           <br />
           <p>
             Welcome to my portfolio, where technology meets passion and faith intertwines with innovation. As a
-            dedicated software engineer and lifelong student, I am committed to pushing the boundaries of what's
+            dedicated software engineer and lifelong student, I am committed to pushing the boundaries of what is
             possible in the digital realm. With a relentless pursuit of knowledge and a fervent belief in the power of
             technology to positively impact our world, I embark on a journey of continual growth and exploration.
           </p>
           <br />
           <p>
-            Within these virtual pages, you'll find a showcase of my projects, each a testament to my skills,
+            Within these virtual pages, you will find a showcase of my projects, each a testament to my skills,
             creativity, and unwavering dedication to excellence. From elegant code solutions to cutting-edge software
             applications, my work reflects both my technical proficiency and my relentless pursuit of excellence.
           </p>
@@ -63,7 +63,7 @@ export default function About() {
           <p>
             But beyond mere technical prowess, my portfolio is infused with a deeper purpose – a steadfast faith that
             guides my every endeavor. As a believer, I approach my work with integrity, humility, and a commitment to
-            using my skills for the greater good. Whether it's developing tools to empower communities, creating
+            using my skills for the greater good. Whether it is developing tools to empower communities, creating
             solutions to tackle pressing societal challenges, or simply striving to make the world a better place, my
             faith serves as a compass, guiding me towards meaningful and impactful work.
           </p>
